1556ea3ad7c12a304a76d34922502936fbc73d75
Nick Malleson <ID+USERNAME@users.noreply.github.com>
2026-02-09 19:08:44 +0000

    replaced old cache with new h5 file

--- llm/python/Embeddings/CLIP/WithClustering/directory_filepaths.py
+++ llm/python/Embeddings/CLIP/WithClustering/directory_filepaths.py
@@ -4,3 +4,5 @@ from pathlib import Path
 data_dir =   os.path.join("../../../../data/embeddings/")
 lsoas_file = os.path.join("../../../../data/SpatialData/", "LSOAs_2021", "LSOA_2021_EW_BSC_V4.shp")
 imd_file = (Path(data_dir) / ".." / "imd" / "File_2_-_IoD2025_Domains_of_Deprivation.xlsx").resolve()
+
+h5_filename = data_dir + "sample_points_cache/street_data.h5"
