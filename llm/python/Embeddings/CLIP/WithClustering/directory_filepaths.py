f3f64bd78ec745993c6a544a3b48f7196a3dcb85
Nick Malleson <ID+USERNAME@users.noreply.github.com>
2026-02-25 14:30:02 +0000

    changed data path (I wasnt sure about the "resolve" bit

--- llm/python/Embeddings/CLIP/WithClustering/directory_filepaths.py
+++ llm/python/Embeddings/CLIP/WithClustering/directory_filepaths.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 data_dir =   os.path.join("../../../../data/embeddings/")
 lsoas_file = os.path.join(data_dir, "SpatialData", "LSOAs_2021", "LSOA_2021_EW_BSC_V4.shp")
-imd_file = (Path(data_dir) / ".." / "imd" / "File_2_-_IoD2025_Domains_of_Deprivation.xlsx").resolve()
+imd_file =   os.path.join(data_dir, "imd", "File_2_-_IoD2025_Domains_of_Deprivation.xlsx")
+#imd_file = (Path(data_dir) / ".." / "imd" / "File_2_-_IoD2025_Domains_of_Deprivation.xlsx").resolve()
 
 h5_filename = data_dir + "sample_points_cache/street_data.h5"
